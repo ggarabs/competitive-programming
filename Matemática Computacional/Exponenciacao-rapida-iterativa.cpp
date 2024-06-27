bbbbc1f1144f595bec8e1c7b33768120fee58348
ggarabs <ggarabetti0607@gmail.com>
2024-06-27 16:54:32 -0300

    Fast iterative exponentiation added in Computational Mathematics

---
+++ Matemática Computacional/Exponenciacao-rapida-iterativa.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define mod %
+#define lli long long int
+
+const int INF = 1e9+7;
+
+// Exponenciação rápida em O(log N)
+
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+
+    while(n--){
+        lli a, b; cin >> a >> b;
+
+        lli ans = 1;
+
+        while(b > 0){
+            if(b & 1) ans = (ans * a) mod INF;
+            b = b >> 1;
+            a = (a * a) mod INF;
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
