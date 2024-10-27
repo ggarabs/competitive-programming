455cac5406fe081030e3a3a40c2e6b3d29d43ba6
gustavo <ggarabetti0607@gmail.com>
2024-10-27 15:41:36 -0300

    fix: iterative TSP fixed

--- Técnicas de Programação/TSP-iterative.cpp
+++ Técnicas de Programação/TSP-iterative.cpp
@@ -1,9 +1,7 @@
 void tsp(int n){
 	dp[1][0] = 0;
 
-	int mask;
-
-	for (mask = 1; mask < (1 << n); mask+=2) {
+	for (int mask = 0; mask < (1 << n); mask++) {
 		for (int i = 0; i < n; i++) {
 			if (!(mask & (1 << i))) continue;
 
